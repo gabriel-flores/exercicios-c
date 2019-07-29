77a2e52a8c28980b0b16bc9fe0dde99380c11e80
Gabriel Flores <gabriel.flores@objectedge.com>
2019-07-29 10:54:50 -0300

    Document the user module

--- 01-arquivos/user.h
+++ 01-arquivos/user.h
@@ -1,2 +1,10 @@
+/*
+ * Prints all users in the database file.
+ */
 void list_users (void);
+
+/*
+ * Validates the input of `name` and `age`, converts them and if valid stores
+ * the user into the database file.
+ */
 void create_user (char *name, char *age);
